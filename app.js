a51ff05b103a31c0316e650c26f86ff50ba0f3a4
ersanjay5991 <39982842+ersanjay5991@users.noreply.github.com>
2023-02-11 17:02:01 +0530

    Create app.js
    this test code for inject store front

---
+++ app.js
@@ -0,0 +1,7 @@
+var button = document.createElement("button");
+button.innerHTML = "S Button";
+button.onclick = function() {
+    alert("Button clicked!");
+}
+;
+document.body.appendChild(button);
