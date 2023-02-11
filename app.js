cb383aeeb49750c635b0aa6522b5e9e10c1f9797
ersanjay5991 <39982842+ersanjay5991@users.noreply.github.com>
2023-02-11 20:06:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ button.onclick = function() {
     alert("Button clicked!");
 }
 ;
+
+// Set styles
+button.style.backgroundColor = "blue";
+button.style.color = "white";
+button.style.border = "2px solid";
+button.style.padding = "10px 20px";
+button.style.borderRadius = "5px";
 document.body.appendChild(button);
